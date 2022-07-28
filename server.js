df3594dd1842db7b935e9a467e13e8afbb9195fa
kimwonjin1 <dnjswls1459@gmail.com>
2022-07-28 22:05:25 +0900

    refund

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-const PORT = process.env.PORT || 5000 
-app.listen(PORT, () => {
+
+app.listen(process.env.PORT || 5000, () => {
     console.log('서버 포트에 연결됨', PORT);
 })
